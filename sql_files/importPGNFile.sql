c5e18f0a8df43c45c6c1b96de237a5542d84df77
sony <sony.sth8@gmail.com>
2023-11-27 09:23:12 +0100

    importPGNFile modified

--- sql_files/importPGNFile.sql
+++ sql_files/importPGNFile.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS chessgame;
 CREATE TABLE chessgame(pgn chess_game);
 
 COPY chessgame(pgn)
-FROM '/mnt/c/ULB/Database Systems Architecture/Project/git/DSA_ChessGame/sample_pgn100000.csv'
+FROM '/mnt/c/ULB/Database Systems Architecture/Project/git/DSA_ChessGame/sample_pgn10000.csv'
 DELIMITER ','
 CSV HEADER;
