12eb9168966bd776f28a9c7afe09fff3057c3b36
sony <sony.sth8@gmail.com>
2023-12-04 22:12:20 +0100

    Issue related to GIN Index fixed

--- chess_game--1.0.sql
+++ chess_game--1.0.sql
@@ -10,10 +10,12 @@ CREATE OR REPLACE FUNCTION chess_board_in(cstring)
   AS 'MODULE_PATHNAME'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+
 CREATE OR REPLACE FUNCTION chess_board_out(chess_board)
   RETURNS cstring
   AS 'MODULE_PATHNAME'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 
 CREATE TYPE chess_board (
   internallength = 69, -- 69 is the length of the `board` type from `smallchesslib.h`
@@ -27,10 +29,12 @@ CREATE OR REPLACE FUNCTION chess_game_in(cstring)
   AS 'MODULE_PATHNAME'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+
 CREATE OR REPLACE FUNCTION chess_game_out(chess_game)
   RETURNS cstring
   AS 'MODULE_PATHNAME'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 
 CREATE TYPE chess_game (
   internallength = 512, -- 512 is the length of the `record` type from `smallchesslib.h`
@@ -38,15 +42,18 @@ CREATE TYPE chess_game (
   output         = chess_game_out
 );
 
+
 CREATE FUNCTION getBoard(chess_game, integer)
   RETURNS chess_board
   AS 'MODULE_PATHNAME', 'getBoard'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+
 CREATE FUNCTION getFirstMoves(chess_game, integer)
   RETURNS chess_game
   AS 'MODULE_PATHNAME', 'getFirstMoves'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 
 CREATE OR REPLACE FUNCTION hasOpening(game chess_game, opening chess_game) 
     RETURNS boolean
@@ -56,10 +63,13 @@ CREATE OR REPLACE FUNCTION hasOpening(game chess_game, opening chess_game)
     LANGUAGE sql;    
 
 
-CREATE FUNCTION hasBoard(chess_game, chess_board,integer)
-  RETURNS boolean
-  AS 'MODULE_PATHNAME', 'hasBoard'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+CREATE OR REPLACE FUNCTION hasBoard(game chess_game, board chess_board,half_move integer) 
+    RETURNS boolean
+    AS $$
+        select game @@ CONCAT(half_move::VARCHAR,',',board::VARCHAR);
+    $$
+    LANGUAGE sql;  
+
 
 CREATE FUNCTION chess_game_lt(chess_game, chess_game)
   RETURNS boolean
@@ -71,6 +81,7 @@ CREATE FUNCTION chess_game_le(chess_game, chess_game)
   RETURNS boolean
   AS 'MODULE_PATHNAME', 'chess_game_le'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 
 CREATE FUNCTION chess_game_eq(chess_game, chess_game)
   RETURNS boolean
@@ -84,11 +95,11 @@ CREATE FUNCTION chess_game_gt(chess_game, chess_game)
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
 
-
 CREATE FUNCTION chess_game_ge(chess_game, chess_game)
   RETURNS boolean
   AS 'MODULE_PATHNAME', 'chess_game_ge'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 
 CREATE FUNCTION chess_contains_func(chess_game, chess_board)
   RETURNS boolean
@@ -96,11 +107,18 @@ CREATE FUNCTION chess_contains_func(chess_game, chess_board)
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
 
+CREATE FUNCTION chess_contains_within_func(chess_game, text)
+  RETURNS boolean
+  AS 'MODULE_PATHNAME', 'chess_contains_within_func'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+
 CREATE FUNCTION chess_equals_func(chess_game, chess_game)
   RETURNS boolean
   AS 'MODULE_PATHNAME', 'chess_equals_func'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-  
+
+
 CREATE OPERATOR = (
   LEFTARG  = chess_game,
   RIGHTARG = chess_game,
@@ -108,11 +126,13 @@ CREATE OPERATOR = (
   COMMUTATOR = =
 );
 
+
 CREATE OPERATOR < (
   LEFTARG  = chess_game,
   RIGHTARG = chess_game,
   PROCEDURE = chess_game_lt
 );
+
 
 CREATE OPERATOR <= (
   LEFTARG  = chess_game,
@@ -121,11 +141,13 @@ CREATE OPERATOR <= (
   COMMUTATOR = >=
 );
 
+
 CREATE OPERATOR > (
   LEFTARG  = chess_game,
   RIGHTARG = chess_game,
   PROCEDURE = chess_game_gt
 );
+
 
 CREATE OPERATOR >= (
   LEFTARG  = chess_game,
@@ -133,6 +155,7 @@ CREATE OPERATOR >= (
   PROCEDURE = chess_game_ge,
   COMMUTATOR = <=
 );
+
 
 CREATE OR REPLACE FUNCTION chess_game_cmp(chess_game, chess_game)
   RETURNS integer
@@ -151,15 +174,15 @@ AS
         FUNCTION        1       chess_game_cmp(chess_game, chess_game);
 
 
-
 CREATE FUNCTION chess_game_extractValue(chess_game,internal)
 RETURNS internal AS 'MODULE_PATHNAME', 'chess_game_extractValue'
 LANGUAGE C STRICT;
 
 
-CREATE FUNCTION chess_game_extractQuery(chess_game,internal,internal,internal,internal,internal,internal)
-RETURNS chess_board AS 'MODULE_PATHNAME', 'chess_game_extractQuery'
+CREATE FUNCTION chess_game_extractQuery(text,internal,internal,internal,internal,internal,internal)
+RETURNS internal AS 'MODULE_PATHNAME', 'chess_game_extractQuery'
 LANGUAGE C STRICT;
+
 
 CREATE FUNCTION chess_game_consistent(internal,internal,internal,internal,internal,internal)
 RETURNS internal AS 'MODULE_PATHNAME', 'chess_game_consistent'
@@ -170,18 +193,16 @@ CREATE FUNCTION chess_board_compare(chess_board,chess_board)
 RETURNS int AS 'MODULE_PATHNAME', 'chess_board_compare'
 LANGUAGE C STRICT;
 
--- "contains" operator is often represented as @>
-CREATE OPERATOR @>
-(PROCEDURE = chess_contains_func, LEFTARG = chess_game, RIGHTARG = chess_board);
 
--- CREATE OPERATOR == 
--- (PROCEDURE = chess_equals_func, LEFTARG = chess_game, RIGHTARG = chess_game);
+CREATE OPERATOR @@
+(PROCEDURE = chess_contains_within_func, LEFTARG = chess_game, RIGHTARG = text);
+
 
 CREATE OPERATOR CLASS chessgame_ops
 DEFAULT FOR TYPE chess_game USING gin AS
-    OPERATOR        1       @>(chess_game, chess_board),
+    OPERATOR        1       @@(chess_game,text),
     FUNCTION		    1		    chess_board_compare(chess_board, chess_board),
     FUNCTION        2       chess_game_extractValue(chess_game,internal),
-    FUNCTION        3       chess_game_extractQuery(chess_game,internal,internal,internal,internal,internal,internal),
+    FUNCTION        3       chess_game_extractQuery(text,internal,internal,internal,internal,internal,internal),
     FUNCTION        4       chess_game_consistent(internal,internal,internal,internal,internal,internal),
     STORAGE chess_board;
--- chess_game.c
+++ chess_game.c
@@ -10,6 +10,7 @@
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
 
+
 PG_MODULE_MAGIC;
 
 #define DatumGetBoardP(X) ((SCL_Board *)DatumGetPointer(X))
@@ -22,6 +23,8 @@ PG_MODULE_MAGIC;
 #define PG_GETARG_GAME_P(n) DatumGetGameP(PG_GETARG_DATUM(n))
 #define PG_RETURN_GAME_P(x) return GamePGetDatum(x)
 
+
+// Input Function for chess_board Data Type
 PG_FUNCTION_INFO_V1(chess_board_in);
 Datum chess_board_in(PG_FUNCTION_ARGS)
 {
@@ -30,12 +33,14 @@ Datum chess_board_in(PG_FUNCTION_ARGS)
     SCL_Board *board = palloc(sizeof(SCL_Board));
 
     if (SCL_boardFromFEN(board, str) == 0)
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("invalid board state")));
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("Invalid board state")));
 
     PG_FREE_IF_COPY(str, 0);
     PG_RETURN_BOARD_P(board);
 }
 
+
+// Input Function for chess_game Data Type
 PG_FUNCTION_INFO_V1(chess_game_in);
 Datum chess_game_in(PG_FUNCTION_ARGS)
 {
@@ -49,7 +54,8 @@ Datum chess_game_in(PG_FUNCTION_ARGS)
     PG_RETURN_GAME_P(record);
 }
 
-// Function to convert Chess Board to string
+
+// Function to convert chess_board to string
 char *chess_board_to_str(SCL_Board board)
 {
     char *str = palloc(sizeof(char) * 256);
@@ -60,6 +66,8 @@ char *chess_board_to_str(SCL_Board board)
     }
 }
 
+
+// Output Function for chess_board Data Type
 PG_FUNCTION_INFO_V1(chess_board_out);
 Datum chess_board_out(PG_FUNCTION_ARGS)
 {
@@ -72,6 +80,7 @@ Datum chess_board_out(PG_FUNCTION_ARGS)
 
     PG_RETURN_CSTRING(str);
 }
+
 
 char str[4096];
 void putCharStr(char c)
@@ -86,9 +95,11 @@ void putCharStr(char c)
     *(s + 1) = 0;
 }
 
+
+// Function to convert chess_game to string
 char *chess_game_to_str(SCL_Record record)
 {
-    ereport(WARNING, errmsg_internal("chess_game_to_str"));
+    ereport(WARNING, errmsg_internal("--> FUNCTION chess_game_to_str"));
     char *game = palloc(sizeof(char) * 4096);
     SCL_printPGN(record, putCharStr, 0);
     for (int i = 0; i < 4096; i++)
@@ -104,6 +115,8 @@ char *chess_game_to_str(SCL_Record record)
     return game;
 }
 
+
+// Output Function for chess_game Data Type
 PG_FUNCTION_INFO_V1(chess_game_out);
 Datum chess_game_out(PG_FUNCTION_ARGS)
 {
@@ -118,16 +131,14 @@ Datum chess_game_out(PG_FUNCTION_ARGS)
         else
             game[i] = str[i];
     }
-
     str[0] = '\0';
-    // game = str[4096];
     PG_RETURN_CSTRING(game);
 }
 
+
+// Function to get initial board state
 SCL_Board *get_starting_board()
 {
-    ereport(WARNING, errmsg_internal("get_starting_board"));
-    // TODO: should we use `palloc` for the `board`? since it's being freed by postgres
     SCL_Board *board = palloc(sizeof(SCL_Board));
     char *str = palloc(sizeof(char) * 256);
 
@@ -138,22 +149,22 @@ SCL_Board *get_starting_board()
     return board;
 }
 
+
+// Function to return chess_board after apply n half_move in given chess_game
 SCL_Board *get_board_internal(SCL_Record record, int half_moves)
 {
-    ereport(WARNING, errmsg_internal("get_board_internal"));
     SCL_Board *board = get_starting_board();
     SCL_recordApply(record, board, half_moves);
     char *str=chess_board_to_str(board);
-    ereport(WARNING, errmsg_internal("%s",str));
     pfree(str);
-
     return board;
 }
 
+
+// Function to get chess_board after apply n half_move in given chess_game
 PG_FUNCTION_INFO_V1(getBoard);
 Datum getBoard(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("getBoard"));
     SCL_Record *record = PG_GETARG_GAME_P(0);
     int half_move = PG_GETARG_INT32(1);
 
@@ -162,9 +173,9 @@ Datum getBoard(PG_FUNCTION_ARGS)
     PG_RETURN_BOARD_P(board);
 }
 
+
 void truncate_pgn_internal(char *chess_notation, int half_moves, char *result_board)
 {
-    ereport(WARNING, errmsg_internal("truncate_pgn_internal"));
     int curr_str_idx = 0;
     int curr_half_move = 0;
 
@@ -210,10 +221,11 @@ void truncate_pgn_internal(char *chess_notation, int half_moves, char *result_bo
     result_board[curr_str_idx] = '\0';
 }
 
+
+// Function to get first n half moves from chess_game
 PG_FUNCTION_INFO_V1(getFirstMoves);
 Datum getFirstMoves(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("getFirstMoves"));
     SCL_Record *record = PG_GETARG_GAME_P(0);
     int half_move = PG_GETARG_INT32(1);
     char *chess_game_str;
@@ -226,11 +238,11 @@ Datum getFirstMoves(PG_FUNCTION_ARGS)
     PG_RETURN_GAME_P(output_record);
 }
 
+
+// Function to compare two string values
 int compare_strings(char *str1, char *str2)
 {
-    ereport(WARNING, errmsg_internal("compare_strings"));
     // Check if str1 starts with str2 (ignoring the last character because it's an added *)
-    // TODO: remove the -1 since we ignore the * now
     if (strncmp(str1, str2, strlen(str2) - 1) == 0)
     {
         return true;
@@ -241,38 +253,11 @@ int compare_strings(char *str1, char *str2)
     }
 }
 
-PG_FUNCTION_INFO_V1(hasBoard);
-Datum hasBoard(PG_FUNCTION_ARGS)
-{
-    ereport(WARNING, errmsg_internal("hasBoard"));
-    SCL_Record *record = PG_GETARG_GAME_P(0);
-    SCL_Board *board = PG_GETARG_BOARD_P(1);
-    int half_move = PG_GETARG_INT32(2);
 
-    char *board_state1 = chess_board_to_str(board);
-    int index1 = strcspn(board_state1, " ");
-
-    for (int i = 0; i <= half_move; i++)
-    {
-        SCL_Board *result_board_state = get_board_internal(*record, i);
-
-        char *board_state2 = chess_board_to_str(result_board_state);
-
-        // TODO: SCL_boardsDiffer does not ignore the things after the board state
-        // is there a function that does that? if not we can do our own?
-        if (strncmp(board_state1, board_state2, index1) == 0)
-        {
-            PG_RETURN_BOOL(true);
-        }
-    }
-
-    PG_RETURN_BOOL(false);
-}
-
+// Function to compare two chess_game
 static int
 compare_games(SCL_Record *c, SCL_Record *d)
 {
-    ereport(WARNING, errmsg_internal("compare_games"));
     char *chess_game_str1;
     char *chess_game_str2;
 
@@ -282,10 +267,11 @@ compare_games(SCL_Record *c, SCL_Record *d)
     return strcmp(chess_game_str1, chess_game_str2);
 }
 
+
+// Function to check if first chess_game is less than second one
 PG_FUNCTION_INFO_V1(chess_game_lt);
 Datum chess_game_lt(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_lt"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -301,10 +287,11 @@ Datum chess_game_lt(PG_FUNCTION_ARGS)
     PG_RETURN_BOOL(result);
 }
 
+
+// Function to check if first chess_game is less than equal to second one
 PG_FUNCTION_INFO_V1(chess_game_le);
 Datum chess_game_le(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_le"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -312,14 +299,14 @@ Datum chess_game_le(PG_FUNCTION_ARGS)
 
     PG_FREE_IF_COPY(c, 0);
     PG_FREE_IF_COPY(d, 1);
-    ereport(WARNING, errmsg_internal("inside if %d", result));
     PG_RETURN_BOOL(result);
 }
 
+
+// Function to check if first chess_game is equal to second one
 PG_FUNCTION_INFO_V1(chess_game_eq);
 Datum chess_game_eq(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_eq"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -330,10 +317,11 @@ Datum chess_game_eq(PG_FUNCTION_ARGS)
     PG_RETURN_BOOL(result);
 }
 
+
+// Function to check if first chess_game is greater than second one
 PG_FUNCTION_INFO_V1(chess_game_gt);
 Datum chess_game_gt(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_gt"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -344,10 +332,11 @@ Datum chess_game_gt(PG_FUNCTION_ARGS)
     PG_RETURN_BOOL(result);
 }
 
+
+// Function to check if first chess_game is greater than equal to second one
 PG_FUNCTION_INFO_V1(chess_game_ge);
 Datum chess_game_ge(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_ge"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -358,10 +347,14 @@ Datum chess_game_ge(PG_FUNCTION_ARGS)
     PG_RETURN_BOOL(result);
 }
 
+
+// Function to compare two chess_game
+// 0 - two chess game are equal
+// -1 - first chess_game is smaller than the second one
+// 1 - first chess_game is larger than the second one
 PG_FUNCTION_INFO_V1(chess_game_cmp);
 Datum chess_game_cmp(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_cmp"));
     SCL_Record *c = PG_GETARG_GAME_P(0);
     SCL_Record *d = PG_GETARG_GAME_P(1);
 
@@ -378,110 +371,57 @@ Datum chess_game_cmp(PG_FUNCTION_ARGS)
     PG_RETURN_INT32(result);
 }
 
-// ------------------------------------------------------------------------------------------- //
-// checks if the first chessgame contains all moves from the second chessgame
 
-PG_FUNCTION_INFO_V1(chess_contains_func);
-
-Datum chess_contains_func(PG_FUNCTION_ARGS)
+// Function to check if chess_game contains given chess_board within first n half moves
+PG_FUNCTION_INFO_V1(chess_contains_within_func);
+Datum chess_contains_within_func(PG_FUNCTION_ARGS)
 {
     SCL_Record *game = PG_GETARG_GAME_P(0);
-    SCL_Board *board = PG_GETARG_BOARD_P(1);
+    char *gm=chess_game_to_str(game);
 
-    char *board_state1 = chess_board_to_str(board);
+    text *text_str = PG_GETARG_TEXT_PP(1);
+    char *str = text_to_cstring(text_str);
+
+    // We get input in the form of half_move,chess_board
+    // Tokenize to extract value of half_move and chess_board
+    char *token = strtok(str, ",");
+    int check_till = atoi(token); // First token is an integer
+    token = strtok(NULL, ",");
+    char *board_state1=palloc(sizeof(char) * 256);  // Second token is a string
+    strcpy(board_state1, token);
+
+    // Considering that we only need to consider board state
     int index1 = strcspn(board_state1, " ");
-    ereport(WARNING, errmsg_internal("index1: %d", index1));
-
-    int half_move = count_half_moves(game);
-    bool result = false;
-
-    for (int i = 0; i <= half_move; i++)
+    bool result;
+    
+    for (int i = 0; i <= check_till; i++)
     {
-        ereport(WARNING, errmsg_internal("Loop: %d", i));
         SCL_Board *result_board_state = get_board_internal(*game, i);
         char *board_state2 = chess_board_to_str(result_board_state);
 
-        ereport(WARNING, errmsg_internal("Testing 2 %s", board_state2));
-        ereport(WARNING, errmsg_internal("Testing 21 %s", board_state1));
-
-        if (strncmp(board_state1, board_state2, index1) == 0)
+        result=strncmp(board_state1, board_state2, index1) == 0;
+        if (result)
         {
-            ereport(WARNING, errmsg_internal("inside if"));
-            result = true;
-            ereport(WARNING, errmsg_internal("inside if %d", result));
             break;
         }
-
-        pfree(board_state2);
-        pfree(result_board_state);
     }
 
-    pfree(board_state1);
-
-    PG_FREE_IF_COPY(game, 0);
-    PG_FREE_IF_COPY(board, 1);
-
-    ereport(WARNING, errmsg_internal("Result: %d", result));
-
-    PG_RETURN_BOOL(1==1);
-}
-
-
-PG_FUNCTION_INFO_V1(chess_equals_func);
-
-Datum chess_equals_func(PG_FUNCTION_ARGS)
-{
-    ereport(WARNING, errmsg_internal("chess_equals_func"));
-    SCL_Record *c = PG_GETARG_GAME_P(0);
-    SCL_Record *d = PG_GETARG_GAME_P(1);
-
-    // Assuming compare_games function returns 0 if games are equal, and non-zero otherwise
-    int diff = compare_games(c, d);
-
-    // Check if the games are equal
-    bool result = (diff == 0);
-
-    PG_FREE_IF_COPY(c, 0);
-    PG_FREE_IF_COPY(d, 1);
-
+    PG_FREE_IF_COPY(game,0);
+    PG_FREE_IF_COPY(str,1);
     PG_RETURN_BOOL(result);
 }
 
-PG_FUNCTION_INFO_V1(chess_move_compare);
 
-Datum chess_move_compare(PG_FUNCTION_ARGS)
-{
-    ereport(WARNING, errmsg_internal("chess_move_compare"));
-    SCL_Record *c = PG_GETARG_GAME_P(0);
-    SCL_Record *d = PG_GETARG_GAME_P(1);
-
-    int diff = compare_games(c, d);
-
-    int result = 0;
-    if (diff < 0)
-        result = -1;
-    else if (diff > 0)
-        result = 1;
-
-    PG_FREE_IF_COPY(c, 0);
-    PG_FREE_IF_COPY(d, 1);
-    PG_RETURN_INT32(result);
-}
-
+// Function to compare two chess_board
 PG_FUNCTION_INFO_V1(chess_board_compare);
-
 Datum chess_board_compare(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_board_compare"));
     SCL_Board *c = PG_GETARG_BOARD_P(0);
     SCL_Board *d = PG_GETARG_BOARD_P(1);
 
     char *board1=chess_board_to_str(c);
     char *board2=chess_board_to_str(d);
     
-//    printf("%s",board1);
-//    printf("%s",board2);
-
     int diff = compare_strings(board1, board2);
 
     int result = 0;
@@ -495,6 +435,9 @@ Datum chess_board_compare(PG_FUNCTION_ARGS)
     PG_RETURN_INT32(result);
 }
 
+
+// Count number of commas in a string
+// Used when we want to count the number of moves in chess game
 int countCommas(char *str)
 {
     int count = 0;
@@ -509,57 +452,47 @@ int countCommas(char *str)
     return count;
 }
 
-//
-//parses a string of chess game notation, extracts individual moves (ignoring the move numbers), 
-//and stores them in an array. This array, along with the count of moves, is then returned. 
-//This function is particularly tailored for use in a GIN index, where it would be used to extract 
-//indexable elements (the moves) from the stored data (the game notation
 
+// extractQuery function used by GIN Index
 PG_FUNCTION_INFO_V1(chess_game_extractQuery);
-
 Datum chess_game_extractQuery(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_extractQuery"));
-    SCL_Board *board = PG_GETARG_BOARD_P(0);
-    int32_t *nkeys = (int32_t *) PG_GETARG_POINTER(1);
-
+    text *text_str = PG_GETARG_TEXT_PP(0);
+    int32 *nkeys = (int32 *) PG_GETARG_POINTER(1);
 	StrategyNumber strategy = PG_GETARG_UINT16(2);
+	bool **pmatch = (bool **) PG_GETARG_POINTER(3); 
+	Pointer *extra_data = (Pointer *) PG_GETARG_POINTER(4); 
+	bool **nullFlags = (bool **) PG_GETARG_POINTER(5);
+	int32 *searchMode = (int32 *) PG_GETARG_POINTER(6);
 
-	bool   **pmatch = (bool **) PG_GETARG_POINTER(3); 
-	Pointer	   *extra_data = (Pointer *) PG_GETARG_POINTER(4); 
-	bool	  **nullFlags = (bool **) PG_GETARG_POINTER(5);
-	int32	   *searchMode = (int32 *) PG_GETARG_POINTER(6);
+    // Create an array of datum type
+    Datum *boards = (Datum *) palloc(sizeof(Datum));
+
+    // Convert second parameter text to string
+    char *str = text_to_cstring(text_str);
+
+    // String contains value in the form of half_move,chess_board
+    // Tokenize string to extract value of half_move and chess_board
+    char *token = strtok(str, ",");
+    int check_till = atoi(token); // First token is an integer
+    token = strtok(NULL, ",");
+    char *board_state1=palloc(sizeof(char) * 256);  // Second toekn is a string
+    strcpy(board_state1, token);
+
+    // Convert string fen to chess_board data type
+    SCL_Board *board = palloc(sizeof(SCL_Board));;
+    SCL_boardFromFEN(board, board_state1);
+    boards[0] = PointerGetDatum(board);
 
     *searchMode = GIN_SEARCH_MODE_DEFAULT;
-
-    // Make a copy of the input string because strtok modifies the string
-    // char *inputCopy = strdup(query_str);
-
-    // int total_boards = countCommas(query_str)+1;
-    
-    // char *keys = (char **) malloc(total_boards * sizeof(char));
-    // int counter = 0;
-
-    // // Tokenize the string
-    // const char delim[2] = ",";
-    // char *token = strtok(inputCopy, delim);
-
-    // while (token != NULL) {
-    //     if ((keys[counter] = (char *)malloc(strlen(token) + 1)) != NULL) {
-    //         strcpy(keys[counter], token);
-    //     }
-    //     counter+=1;
-    //     token = strtok(NULL, delim);
-    // }
-
     *nkeys = 1;
-    // PG_FREE_IF_COPY(query_text, 0);
-    PG_RETURN_BOARD_P(board);
+    PG_RETURN_POINTER(boards);
 }
 
+
+// Function to return number of half moves of chess_game
 int count_half_moves(SCL_Record record)
 {
-    ereport(WARNING, errmsg_internal("count_half_moves"));
     char *chess_game_str = chess_game_to_str(record);
 
     // Initialize the counter for half-moves
@@ -586,11 +519,11 @@ int count_half_moves(SCL_Record record)
     return total_half_moves;
 }
 
-PG_FUNCTION_INFO_V1(chess_game_extractValue);
 
+// extractValue function for GIN Index
+PG_FUNCTION_INFO_V1(chess_game_extractValue);
 Datum chess_game_extractValue(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_extractValue"));
     SCL_Record *record = PG_GETARG_GAME_P(0);
     int32 *nkeys = (int32 *) PG_GETARG_POINTER(1);
     int total_half_moves = count_half_moves(record)+1;
@@ -613,11 +546,11 @@ Datum chess_game_extractValue(PG_FUNCTION_ARGS)
 }
 
 
-PG_FUNCTION_INFO_V1(chess_game_consistent);
 
+// consistent function for GIN Index
+PG_FUNCTION_INFO_V1(chess_game_consistent);
 Datum chess_game_consistent(PG_FUNCTION_ARGS)
 {
-    ereport(WARNING, errmsg_internal("chess_game_consistent"));
     // Retrieve the array indicating which query keys are present in the indexed value
     bool *check = (bool *) PG_GETARG_POINTER(0);
     // Retrieve the strategy number
@@ -647,10 +580,10 @@ Datum chess_game_consistent(PG_FUNCTION_ARGS)
 
     // No keys matched, or for other unhandled strategies, return false
     *recheck = false;
-    // PG_FREE_IF_COPY(check, 0);
-    // PG_FREE_IF_COPY(strategy, 1);
-    // PG_FREE_IF_COPY(query, 2);
-    // PG_FREE_IF_COPY(extra_data,4);
-    // PG_FREE_IF_COPY(recheck,5);
+    PG_FREE_IF_COPY(check, 0);
+    PG_FREE_IF_COPY(strategy, 1);
+    PG_FREE_IF_COPY(query, 2);
+    PG_FREE_IF_COPY(extra_data,4);
+    PG_FREE_IF_COPY(recheck,5);
     PG_RETURN_BOOL(false);
 }
